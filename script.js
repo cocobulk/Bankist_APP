2b12e780fdae453139577fdb4815976d0f101e40
Corentin Bulte <corentinbulte@gmail.com>
2023-10-04 13:07:05 +0200

    movement function

--- script.js
+++ script.js
@@ -61,9 +61,27 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${mov}</div>
+      </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
+
+/* LECTURES
 
 const currencies = new Map([
   ['USD', 'United States dollar'],
@@ -81,4 +99,4 @@ movements.forEach(function (movement, i) {
   }
 });
 
-/////////////////////////////////////////////////
+*/
